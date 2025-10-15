9d824eee9019e3bf96425b432da9d55c3ecb06aa
Nazar Artemenkov <bima62374@gmail.com>
2025-10-15 16:15:43 +0300

    Fix last pull request

--- src/main/java/de/upteams/tasktracker/project/constants/ProjectValidationConstats.java
+++ src/main/java/de/upteams/tasktracker/project/constants/ProjectValidationConstats.java
@@ -6,7 +6,7 @@ public final class ProjectValidationConstats {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String NAME_REGEX = "[^[A-Z][a-zA-Z0-9 ]{2,154}$]";
+    public static final String NAME_REGEX = "^[A-Z][a-zA-Z0-9 ]{2,154}$";
     public static final int NAME_MIN_LENGTH = 3;
     public static final int NAME_MAX_LENGTH = 155;
 }
